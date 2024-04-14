def915c9e17ce0b2eeaa742d01b719df23d8091a
dadams2001 <dadams22@nd.edu>
2024-04-14 00:28:15 -0400

    backend pulling works

--- src/Components/FlavorProfile.js
+++ src/Components/FlavorProfile.js
@@ -3,36 +3,22 @@ import SelectedFood from "./SelectedFood";
 import Autosuggest from "react-autosuggest";
 const FlavorProfile = () => {
     const [suggestions, setSuggestions] = useState([]);
-    const [newDish, setNewDish] = useState({});
     const [myDishes, setMyDishes] = useState([]);
     const [myValue, setMyValue] = useState("");
 
     useEffect(() => {
-        const dishes = [
-            {
-                id: 0,
-                title: "dish1",
-                territory: "USA babyyyy"
-            },
-            {
-                id: 1,
-                title: "dish2",
-                territory: "USA babyyyy"
-            },
-            {
-                id: 2,
-                title: "dish3",
-                territory: "USA babyyyy"
-            }
-        ];
-        setSuggestions(dishes.filter(dish => dish.title.includes(myValue)));
+        fetch(`http://10.0.0.250:8000/dishes/search/${myValue.replace(/\s/g, "_")}/5`)
+            .then(response => response.json())
+            .then(data => {
+                setSuggestions(data)
+            })
     }, [myValue]);
 
-    useEffect(() => {
-        if (Object.keys(newDish).length !== 0) {
-            setMyDishes([...myDishes, newDish]);
+    const updateMyDishes = (input) => {
+        if (Object.keys(input).length !== 0) {
+            setMyDishes([...myDishes, input]);
         }
-    }, [newDish]);
+    };
 
     const removeDish = (id) => {
         setMyDishes(myDishes.filter(dish => dish.id !== id));
@@ -44,8 +30,8 @@ const FlavorProfile = () => {
 
     // autosuggest functions
     const renderSuggestion = suggestion => (
-        <div key={suggestion.id} onClick={() => setNewDish(suggestion)}>
-          {suggestion.title}
+        <div key={suggestion.id} onClick={() => updateMyDishes(suggestion)}>
+          {suggestion.name}
         </div>
     );
  
