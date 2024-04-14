7004f2b2890cbbcada3a9e296fae460cf2829c18
jsteve22 <jsteve22@nd.edu>
2024-04-13 21:58:02 -0400

    edited panels

--- src/Components/FoodPanel.js
+++ src/Components/FoodPanel.js
@@ -9,8 +9,8 @@ const FoodPanel = ({id, name, img, description, uri}) => {
     }
 
     return(
-      <div className="w-72 h-96 m-16 bg-my-dark-green rounded-lg flex flex-col items-center text-white"> 
-        <img src={img} alt="food" className="w-7/12  h-2/5 mt-4"/>
+      <div className="w-72 h-96 m-16 bg-my-dark-green rounded-lg flex flex-col items-center text-white opacity-90"> 
+        <img src={img} alt="food" className="w-7/12  h-2/5 mt-8"/>
         <div className="font-bold text-xl py-2">
           <Link to={uri} target="_blank" className="hover:text-my-orange">
             {name} 
