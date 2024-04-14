f2533432fce16fa577328eb0e37ae03c46cf2cb0
jsteve22 <jsteve22@nd.edu>
2024-04-14 11:27:44 -0400

    add links to Food Recs

--- src/Components/FoodRecs.js
+++ src/Components/FoodRecs.js
@@ -2,10 +2,13 @@ import { Link } from 'react-router-dom';
 
 const FoodRecs = ({id, name, img, ingredients, uri}) => {
 
-  const likes = ['mutton', 'turmeric', 'beef', 'cumin', 'okra leaves'];
+  const likes = ['beef stock', 'vinegar', 'pork ribs', 'onions', 'herbs', 'lamb', 'salt', 'cumin seed', 'vegetable oil', 'red wine vinegar', 'kosher salt', 'orange juice', 'ground cumin', 'chicken', 'oregano', 'dried oregano', 'garlic', 'cloves', 'scallions', 'pepper', 'mutton', 'turmeric', 'paprika', 'flour', 'mango grilling glaze', 'sugar', 'chile powder'];
+  // const likes = ['mutton', 'turmeric', 'beef', 'cumin', 'okra leaves'];
   // ingredients: chicken, mutton, tumeric
   // dishes: pie, pollo asada, grilled pork ribs
   // exclusions: peanuts
+
+  const recipe_links = ['https://everylittlecrumb.com/mulukhiyah/','https://www.alphafoodie.com/sfiha-lebanese-meat-pies/','https://www.recipetineats.com/chicken-sharwama-middle-eastern/'];
 
   return(
     <Link to={uri} target="_blank" className="w-72 mx-16 my-8 rounded-lg flex flex-col items-center text-[#4c651e]"> 
@@ -43,6 +46,11 @@ const FoodRecs = ({id, name, img, ingredients, uri}) => {
               </span>
           );
         })}
+      </div>
+      <div className="text-my-dark-purple hover:text-my-orange">
+        <Link to={recipe_links[id]} target="_blank">
+          [Click Here for a Recipe]
+        </Link>
       </div>
     </Link>
   );
