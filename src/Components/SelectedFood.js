ce0be53a77f30dfe4dffbda2712a7dde9d38385d
dadams2001 <dadams22@nd.edu>
2024-04-14 00:28:15 -0400

    backend pulling works

--- src/Components/SelectedFood.js
+++ src/Components/SelectedFood.js
@@ -3,12 +3,12 @@ const SelectedFood = ({id, item, removeDish}) => {
         <div className="flex flex-row justify-center h-12 my-2 border-2" key={id}>
             <img 
                 alt="jerbear" 
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuxVT_sCuN7ZS4qERjiX5lDyV5SV-azuOnFBpY73zp8w&s"
+                src={item.image_url}
                 className="h-full mx-1"
             />
             <div className="flex flex-col items-start mx-1">
-                <h3 className="font-bold">{item.title}</h3>
-                <p>{item.territory}</p>
+                <h3 className="font-bold">{item.name}</h3>
+                <p>{item.cuisine_type}</p>
             </div>
             <div className="flex flex-col justify-center mx-1" onClick={() => removeDish(item.id)}>
                 <p className="font-bold" >
